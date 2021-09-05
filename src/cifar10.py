611f0747247a8cfcf1ed2ed9ae6744a357ccdce0
Ojas Patel <32349027+patelojas@users.noreply.github.com>
2021-09-05 18:44:29 -0500

    Python cifar10 file

---
+++ src/cifar10.py
@@ -0,0 +1,148 @@
+import torch
+import torchvision
+import numpy as np
+import matplotlib.pyplot as plt
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision.datasets import CIFAR10
+from torchvision.transforms import ToTensor
+from torchvision.utils import make_grid
+from torch.utils.data.dataloader import DataLoader
+from torch.utils.data import random_split
+
+def accuracy(outputs, labels):
+    _, preds = torch.max(outputs, dim=1)
+    return torch.tensor(torch.sum(preds == labels).item() / len(preds))
+
+class ImageClassificationBase(nn.Module):
+    def training_step(self, batch):
+        images, labels = batch 
+        out = self(images)                  # Generate predictions
+        loss = F.cross_entropy(out, labels) # Calculate loss
+        return loss
+    
+    def validation_step(self, batch):
+        images, labels = batch 
+        out = self(images)                    # Generate predictions
+        loss = F.cross_entropy(out, labels)   # Calculate loss
+        acc = accuracy(out, labels)           # Calculate accuracy
+        return {'val_loss': loss.detach(), 'val_acc': acc}
+        
+    def validation_epoch_end(self, outputs):
+        batch_losses = [x['val_loss'] for x in outputs]
+        epoch_loss = torch.stack(batch_losses).mean()   # Combine losses
+        batch_accs = [x['val_acc'] for x in outputs]
+        epoch_acc = torch.stack(batch_accs).mean()      # Combine accuracies
+        return {'val_loss': epoch_loss.item(), 'val_acc': epoch_acc.item()}
+    
+    def epoch_end(self, epoch, result):
+        print("Epoch [{}], val_loss: {:.4f}, val_acc: {:.4f}".format(epoch, result['val_loss'], result['val_acc']))
+
+def evaluate(model, val_loader):
+    outputs = [model.validation_step(batch) for batch in val_loader]
+    return model.validation_epoch_end(outputs)
+
+def fit(epochs, lr, model, train_loader, val_loader, opt_func=torch.optim.SGD):
+    history = []
+    optimizer = optim.SGD(model.parameters(), lr=lr)
+    for epoch in range(epochs):
+        # Training Phase 
+        for batch in train_loader:
+            loss = model.training_step(batch)
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+        # Validation phase
+        result = evaluate(model, val_loader)
+        model.epoch_end(epoch, result)
+        history.append(result)
+    return history
+
+def to_device(data, device):
+    """Move tensor(s) to chosen device"""
+    if isinstance(data, (list,tuple)):
+        return [to_device(x, device) for x in data]
+    return data.to(device, non_blocking=True)
+
+class DeviceDataLoader():
+    """Wrap a dataloader to move data to a device"""
+    def __init__(self, dl, device):
+        self.dl = dl
+        self.device = device
+        
+    def __iter__(self):
+        """Yield a batch of data after moving it to device"""
+        for b in self.dl: 
+            yield to_device(b, self.device)
+
+    def __len__(self):
+        """Number of batches"""
+        return len(self.dl)
+
+
+def plot_losses(history):
+    losses = [x['val_loss'] for x in history]
+    plt.plot(losses, '-x')
+    plt.xlabel('epoch')
+    plt.ylabel('loss')
+    plt.title('Loss vs. No. of epochs');
+
+def plot_accuracies(history):
+    accuracies = [x['val_acc'] for x in history]
+    plt.plot(accuracies, '-x')
+    plt.xlabel('epoch')
+    plt.ylabel('accuracy')
+    plt.title('Accuracy vs. No. of epochs');
+
+class CIFAR10Model(ImageClassificationBase):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+        
+    def forward(self, x):
+        # Flatten images into vectors
+        m = nn.GELU() # can change to ELU or Mish
+        x = self.pool(m(self.conv1(x)))
+        x = self.pool(m(self.conv2(x)))
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = m(self.fc1(x))
+        x = m(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+if __name__ == "main":
+    dataset = CIFAR10(root='data/', download=True, transform=ToTensor())
+    test_dataset = CIFAR10(root='data/', train=False, transform=ToTensor())
+
+    classes = dataset.classes
+
+    torch.manual_seed(43)
+    val_size = 10000
+    train_size = len(dataset) - val_size
+
+    train_ds, val_ds = random_split(dataset, [train_size, val_size])
+    batch_size = 128
+    train_loader = DataLoader(train_ds, batch_size, shuffle=True, num_workers=4, pin_memory=True)
+    val_loader = DataLoader(val_ds, batch_size*2, num_workers=4, pin_memory=True)
+    test_loader = DataLoader(test_dataset, batch_size*2, num_workers=4, pin_memory=True)
+    device = torch.device('cpu')
+    train_loader = DeviceDataLoader(train_loader, device)
+    val_loader = DeviceDataLoader(val_loader, device)
+    test_loader = DeviceDataLoader(test_loader, device)
+
+    input_size = 3*32*32
+    output_size = 10
+
+    model = to_device(CIFAR10Model(), device)
+    history = [evaluate(model, val_loader)]
+
+    epochs = 25
+    lr = 1e-1
+
+    history += fit(epochs, lr, model, train_loader, val_loader)
+    print(evaluate(model, test_loader))
